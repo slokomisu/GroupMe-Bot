585bfd13fb0e56dda1d1892aac4069c56581a232
Noah Guillory <slokomisu4@gmail.com>
2017-08-13 18:51:57 -0500

    Add callback

--- index.js
+++ index.js
@@ -27,6 +27,22 @@ app.post('/message', async (req, res) => {
   } catch (error) {
     res.json(error);
   }
+});
+
+app.post('/callback', async (req, res) => {
+  console.log(req.body);
+  const { name, text } = req.body
+  const message = `${name} said: ${text}`;
+  const messageRequest = {
+    bot_id: BOT_ID,
+    text: message
+  };
+  try {
+    await axios.post(API_URL, messageRequest);
+    console.log('Sent message: ', message);
+  } catch (error) {
+    console.error('Something went wrong.', error);
+  }
 })
 
 
