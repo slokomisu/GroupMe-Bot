54b2a1ef870a45154df4c56873ef34541160b7e7
Noah Guillory <slokomisu4@gmail.com>
2017-08-13 19:22:32 -0500

    Add @everyone

--- index.js
+++ index.js
@@ -9,44 +9,56 @@ app.use(cors())
 app.use(bodyParser.json());
 
 const BOT_ID = '3fb20f85ce19705f0ec1aa1a3a';
-const API_URL = 'https://api.groupme.com/v3/bots/post'
+const API_URL = 'https://api.groupme.com/v3'
+const GROUP_ID = '32374324'
 
-app.post('/message', async (req, res) => {
-  const { text } = req.body;
+
+app.post('/callback', async (req, res) => {
+  const { text, sender_type } = req.body;
+  if (text.includes('@everyone') || text.includes('@everybody')) {
+    try {
+      const memberList = await getMemberList();
+      await sendMessage(memberList);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+})
+
+
+async function sendMessage(text) {
   const messageRequest = {
     bot_id: BOT_ID,
     text
   }
   try {
-    const request = await axios.post(API_URL, messageRequest);
+    const request = await axios.post(`${API_URL}/bots/post`, messageRequest);
     const successResponse = {
       message: 'Message succesfully sent',
       text
     }
-    res.json(successResponse)
+    console.log(successResponse)
   } catch (error) {
-    res.json(error);
+    console.log(error);
   }
-});
+}
 
-app.post('/callback', async (req, res) => {
-  console.log(req.body);
-  const { name, text, sender_type } = req.body
-
-  if (sender_type !== 'bot') {
-    const message = `${name} said: ${text}`;
-    const messageRequest = {
-      bot_id: BOT_ID,
-      text: message
-    };
-    try {
-      await axios.post(API_URL, messageRequest);
-      console.log('Sent message: ', message);
-    } catch (error) {
-      console.error('Something went wrong.', error);
-    }
-  }
-})
+async function getMemberList(groupId) {
+   try {
+     const response = await axios.get(`${API_URL}/groups/${groupId}`);
+     const group = response.data;
+     const members = group.members;
+     const membersList = members.map(member => {
+       return member.nickname
+     })
+     .map(member => {
+       return `@${member}`;
+     });
+     return membersList.join(' ');
+   } catch (error) {
+     console.log(error);
+   }
+}
 
 
 const port = process.env.PORT || 4000
