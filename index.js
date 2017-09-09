0f0c3e1f8fee73d450d9abdba8b8d50ab1014b13
Noah Guillory <noah@noahguillory.me>
2017-09-09 14:57:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const axios = require('axios');
 const humanizeList = require('humanize-list')
 
 
-if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config()
-}
+
 
 const app = express();
 app.use(helmet())
+app.use(cors())
 app.use(bodyParser.json());
 
 const API_URL = 'https://api.groupme.com/v3'
