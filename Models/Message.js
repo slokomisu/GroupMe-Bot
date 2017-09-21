d52df6ab80b616fe83ee14bbf595a78ff02dfc61
Noah Guillory <noah@noahguillory.me>
2017-09-21 12:17:13 -0500

    Make the time now default for message creation

--- Models/Message.js
+++ Models/Message.js
@@ -6,7 +6,7 @@ const messageSchema = new mongoose.Schema({
     unique: true,
     required: true
   },
-  created_at: Date,
+  created_at: {type: Date, default: Date.now}
   group_id: String,
   name: String,
   sender_id: String,
