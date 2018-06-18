915c77739ca766630d07a292ccd5b8f4a9c1e6a7
Noah Guillory <Noah.Guillory@cerner.com>
2018-06-18 12:23:38 -0500

    Add raven and configure for express

--- src/App.ts
+++ src/App.ts
@@ -1,19 +1,23 @@
 import * as bodyParser from 'body-parser'
 import * as cors from 'cors'
 import * as express from 'express'
+import * as Raven from 'raven';
 import * as helmet from 'helmet'
 import groupMeRoutes from './routes/groupme-routes'
 
 class App {
   public express: express.Application;
+  private raven;
 
   constructor() {
     this.express = express();
     this.initializeMiddleware();
     this.mountRoutes();
+    this.raven = Raven.config(process.env.DSN).install();
   }
 
   private initializeMiddleware() {
+    this.express.use(Raven.errorHandler());
     this.express.use(helmet());
     this.express.use(cors());
     this.express.use(bodyParser.json());
