841b77c46c7eb713b52ec4725b24e25552200ba1
Noah Guillory <Noah.Guillory@cerner.com>
2018-06-20 10:19:34 -0500

    Add request count to logs

--- src/responses/LocationResponseTrigger.ts
+++ src/responses/LocationResponseTrigger.ts
@@ -8,26 +8,26 @@ import * as moment from 'moment';
 export default class LocationResponseTrigger extends BaseTrigger {
   private mapsClient;
 
-  constructor () {
+  constructor() {
     super()
     this.triggerPatterns = [/@location/]
     this.mapsClient = createClient({
       key: process.env.GOOGLE_MAPS_API_KEY,
       Promise: global.Promise
-  })
+    })
   }
 
-  public async respond (message: IGroupMeMessage): Promise<IBotResponse> {
+  public async respond(message: IGroupMeMessage): Promise<IBotResponse> {
     const inFreeLimit = await this.withinFreeLimit();
     if (!inFreeLimit) {
-        return {
-            responseText: 'Cannot make any more Google Maps requests due to free API usage limits'
-        }
+      return {
+        responseText: 'Cannot make any more Google Maps requests due to free API usage limits'
+      }
     }
     const query = message.text.split('@location ')[1];
     let places;
     try {
-      places = await this.mapsClient.places({query}).asPromise();
+      places = await this.mapsClient.places({ query }).asPromise();
       await this.countRequest(message.name);
     } catch (error) {
       Raven.captureException(error);
@@ -43,17 +43,21 @@ export default class LocationResponseTrigger extends BaseTrigger {
   }
 
   private async withinFreeLimit(): Promise<boolean> {
+
+    return await this.todaysRequests() <= parseInt(process.env.REQUEST_LIMIT, 10);
+  }
+
+  private async todaysRequests(): Promise<number> {
     const today = moment().startOf('day');
     const tomorrow = moment(today).endOf('day');
 
-    const todaysRequests = await MapsRequest.find({
-        requestDate: {
-            $gte: today.toDate(),
-            $lt: tomorrow.toDate(),
-        }
-    });
+    return await MapsRequest.find({
+      requestDate: {
+        $gte: today.toDate(),
+        $lt: tomorrow.toDate(),
+      }
+    }).length;
 
-    return todaysRequests.length <= parseInt(process.env.REQUEST_LIMIT, 10);
   }
 
   private buildResponse(placesResults) {
@@ -67,14 +71,15 @@ export default class LocationResponseTrigger extends BaseTrigger {
       responses.push(response);
     }
     return responses.join('\n');
-  } 
+  }
 
   private async countRequest(requestor) {
     await MapsRequest.create({
-        requestEndpoint: 'https://maps.googleapis.com/maps/api/place/textsearch/json',
-        requestor,
-        requestDate: new Date(Date.now()),
+      requestEndpoint: 'https://maps.googleapis.com/maps/api/place/textsearch/json',
+      requestor,
+      requestDate: new Date(Date.now()),
     })
+    console.log(`Request count for today: ${await this.todaysRequests()}`);
   }
 
   private generateDirectionsURL(placeId): String {
